b11ffb9834c1d57465c1d5163b8b49bade655e54
Piotr Murach <pmurach@gmail.com>
2017-02-05 18:12:18 +0000

    Change to rename, speed up comparison and add once, twice, thrice.

--- lib/rspec/benchmark/comparison_matcher.rb
+++ lib/rspec/benchmark/comparison_matcher.rb
@@ -7,17 +7,12 @@ module RSpec
       #
       # @api private
       class Matcher
-        attr_reader :sample,
-                    :amount,
-                    :iterations,
-                    :sample_iterations,
-                    :comparison_type,
-                    :threshold_type
-
-        def initialize(sample, comparison_type, options = {})
+        def initialize(expected, comparison_type, options = {})
           check_comparison(comparison_type)
-          @sample = sample
+          @expected = expected
           @comparison_type = comparison_type
+          @count = 1
+          @count_type = :at_least
         end
 
         # Indicates this matcher matches against a block
@@ -29,40 +24,72 @@ module RSpec
           true
         end
 
+        def matcher_name
+          "perform_#{@comparison_type}_than"
+        end
+
         # @return [Boolean]
         #
         # @api private
         def matches?(block)
-          @amount ||= 1
+          @expected_iterations = ips_for(@expected)
+          @actual_iterations = ips_for(block)
 
-          @sample_iterations = ips_for(sample)
-          @iterations = ips_for(block)
-
-          case threshold_type
+          case @count_type
           when :at_most
             at_most_comparison
           when :exactly
-            exactly_comparison
+            exact_comparison
           else
             default_comparison
           end
         end
 
-        def at_least(amount)
-          @amount = amount
-          @threshold_type = :at_least
+        def convert_count(n)
+          case n
+          when Numeric then n
+          when :once then 1
+          when :twice then 2
+          when :thrice then 3
+          else
+            raise "The #{matcher_name} matcher is not designed to be used " \
+                  "with #{n} count type."
+          end
+        end
+
+        def at_least(n)
+          @count = convert_count(n)
+          @count_type = :at_least
           self
         end
 
-        def at_most(amount)
-          @amount = amount
-          @threshold_type = :at_most
+        def at_most(n)
+          @count = convert_count(n)
+          @count_type = :at_most
           self
         end
 
-        def exactly(amount)
-          @amount = amount
-          @threshold_type = :exactly
+        def exactly(n)
+          @count = convert_count(n)
+          @count_type = :exactly
+          self
+        end
+
+        def once
+          @count_type = :exactly
+          @count = 1
+          self
+        end
+
+        def twice
+          @count_type = :exactly
+          @count = 2
+          self
+        end
+
+        def thrice
+          @count_type = :exactly
+          @count = 3
           self
         end
 
@@ -70,28 +97,32 @@ module RSpec
           self
         end
 
+        # @api private
         def failure_message
           "expected block to #{description}, but #{failure_reason}"
         end
 
+        # @api private
         def failure_message_when_negated
           "expected block not to #{description}, but #{failure_reason}"
         end
 
+        # @api private
         def description
-          if amount == 1
-            "perform #{comparison_type} than passed block"
+          if @count == 1
+            "perform #{@comparison_type} than passed block"
           else
-            "perform #{comparison_type} than passed block " \
-            "#{threshold_type} in #{amount} times"
+            "perform #{@comparison_type} than passed block " \
+            "by #{@count_type} #{@count} times"
           end
         end
 
+        # @api private
         def failure_reason
           if actual < 1
-            "performed slower in #{format('%.2f', (actual**-1))} times"
+            "performed slower by #{format('%.2f', (actual**-1))} times"
           elsif actual > 1
-            "performed faster in #{format('%.2f', actual)} times"
+            "performed faster by #{format('%.2f', actual)} times"
           else
             "performed by the same time"
           end
@@ -102,85 +133,85 @@ module RSpec
         # @return [Boolean]
         # @example
         #   @actual = 40
-        #   @amount = 41
-        #   perform_faster_than { ... }.at_most(@amount).times # => true
+        #   @count = 41
+        #   perform_faster_than { ... }.at_most(@count).times # => true
         #
         #   @actual = 40
-        #   @amount = 41
-        #   perform_faster_than { ... }.at_most(@amount).times # => false
+        #   @count = 41
+        #   perform_faster_than { ... }.at_most(@count).times # => false
         #
         #   @actual = 1/40
-        #   @amount = 41
-        #   perform_slower_than { ... }.at_most(@amount).times # => true
+        #   @count = 41
+        #   perform_slower_than { ... }.at_most(@count).times # => true
         #
         #   @actual = 1/40
-        #   @amount = 41
-        #   perform_slower_than { ... }.at_most(@amount).times # => false
+        #   @count = 41
+        #   perform_slower_than { ... }.at_most(@count).times # => false
         # @api private
         def at_most_comparison
-          if comparison_type == :faster
-            1 < actual && actual < amount
+          if @comparison_type == :faster
+            1 < actual && actual < @count
           else
-            actual < 1 && actual > 1 / amount.to_f
+            actual < 1 && actual > 1 / @count.to_f
           end
         end
 
         # @return [Boolean]
         # @example
         #   @actual = 40.1
-        #   @amount = 40
-        #   perform_faster_than { ... }.exactly(@amount).times # => true
+        #   @count = 40
+        #   perform_faster_than { ... }.exact(@count).times # => true
         #
         #   @actual = 45
-        #   @amount = 40
-        #   perform_faster_than { ... }.exactly(@amount).times # => true
+        #   @count = 40
+        #   perform_faster_than { ... }.exact(@count).times # => true
         #
         #   @actual = 55
-        #   @amount = 40
-        #   perform_faster_than { ... }.exactly(@amount).times # => false
+        #   @count = 40
+        #   perform_faster_than { ... }.exact(@count).times # => false
         # @api private
-        def exactly_comparison
-          (amount - 0.33 .. amount + 0.33).include? actual
+        def exact_comparison
+          (@count - 0.33 .. @count + 0.33).include? actual
         end
 
         # @return [Boolean]
         # @example
         #   @actual = 41
-        #   @amount = 40
+        #   @count = 40
         #   perform_faster_than { ... } # => true
         #
         #   @actual = 41
-        #   @amount = 40
-        #   perform_faster_than { ... }.at_least(@amount).times # => true
+        #   @count = 40
+        #   perform_faster_than { ... }.at_least(@count).times # => true
         #
         #   @actual = 1/40
-        #   @amount = 41
-        #   perform_slower_than { ... }.at_least(@amount).times # => false
+        #   @count = 41
+        #   perform_slower_than { ... }.at_least(@count).times # => false
         #
         #   @actual = 1/41
-        #   @amount = 40
-        #   perform_slower_than { ... }.at_least(@amount).times # => true
+        #   @count = 40
+        #   perform_slower_than { ... }.at_least(@count).times # => true
         # @api private
         def default_comparison
-          if comparison_type == :faster
-            actual / amount > 1
+          if @comparison_type == :faster
+            actual / @count > 1
           else
-            actual / amount < 1 / amount.to_f
+            actual / @count < 1 / @count.to_f
           end
         end
 
         def check_comparison(type)
           [:slower, :faster].include?(type) ||
-            (raise ArgumentError, "comparison_type must be " \
+           (raise ArgumentError, "comparison_type must be " \
                   ":faster or :slower, not `:#{type}`")
         end
 
         def actual
-          iterations / sample_iterations.to_f
+          @actual_iterations / @expected_iterations.to_f
         end
 
         def ips_for(block)
-          bench = ::Benchmark::Perf::Iteration.new
+          bench = ::Benchmark::Perf::Iteration.new(time: 0.2, warmup: 0.1)
           average, stddev, _ = bench.run(&block)
           average + 3 * stddev
         end
--- spec/unit/comparison_matcher_spec.rb
+++ spec/unit/comparison_matcher_spec.rb
@@ -8,56 +8,70 @@ RSpec.describe RSpec::Benchmark::ComparisonMatcher::Matcher do
     }.to raise_error(ArgumentError, /comparison_type must be :faster or :slower, not `:unknown`/)
   end
 
-  describe '#perform_faster_than' do
-    context "expect { ... }.to perform_faster_than(...)" do
-      it "passes if the block performs faster than sample" do
-        expect {
-           1 << 1
-        }.to perform_faster_than { 'x' * 10 * 1024 }
-      end
-
-      it "fails if the block performs slower than sample" do
-        expect {
-          expect {
-            'x' * 10 * 1024
-          }.to perform_faster_than { 1 << 1 }
-        }.to raise_error(/expected block to perform faster than passed block, but performed slower in \d+.\d+ times/)
-      end
+  describe "expect { ... }.to perform_faster_than(...)" do
+    it "passes if the block performs faster than sample" do
+      expect { 1 << 1 }.to perform_faster_than { 'x' * 10 * 1024 }
     end
 
-    context "expect { ... }.to perform_faster_than(...).at_least(...).times" do
-      it "passes if the block performs faster than sample" do
-        expect {
-          1 << 1
-        }.to perform_faster_than { 'x' * 10 * 1024 }.at_least(2).times
-      end
-
-      it "fails if the block performs faster than sample" do
-        expect {
-          expect {
-            'x' * 10 * 1024
-          }.to perform_faster_than { 1 << 1 }.at_least(5).times
-        }.to raise_error(/expected block to perform faster than passed block at_least in \d+ times, but performed slower in \d+.\d+ times/)
-      end
-    end
-
-    context "expect { ... }.not_to perform_faster_than(...)" do
-      it "passes if the block performs slower than sample" do
+    it "fails if the block performs slower than sample" do
+      expect {
         expect {
           'x' * 10 * 1024
-        }.not_to perform_faster_than { 1 << 1 }
+        }.to perform_faster_than { 1 << 1 }
+      }.to raise_error(/expected block to perform faster than passed block, but performed slower by \d+.\d+ times/)
+    end
+
+    context 'with exact count' do
+      it "passes if the block performs faster than sample" do
+        expect { 1 << 1 }.to perform_faster_than { 1 << 1 }.exactly(1).times
       end
 
-      it "fails if the block performs faster than sample" do
+      it "passes if the block performs specified number of times" do
+        expect { 1 << 1 }.to perform_faster_than { 1 << 1 }.once
+      end
+    end
+
+    context "with at_least count" do
+      it "passes if the block performs faster by count times" do
+        expect { 1 << 1 }.to perform_faster_than { 'x' * 10 * 1024 }.at_least(2)
+      end
+
+      it "fails if the block doesn't perform faster by count times" do
+        expect {
+          expect {
+            'x' * 10 * 1024
+          }.to perform_faster_than { 1 << 1 }.at_least(2).times
+        }.to raise_error(/expected block to perform faster than passed block by at_least \d+ times, but performed slower by \d+.\d+ times/)
+      end
+    end
+
+    context "with at_most count" do
+      it "passes if the block performs faster than sample" do
+        expect { 1 << 1 }.to perform_faster_than { 'x' * 10 * 1024 }.at_most(125)
+      end
+
+      it "fails if the block performs faster than sample more than in 20 times" do
         expect {
           expect {
             1 << 1
-          }.not_to perform_faster_than { 'x' * 10 * 1024 }
-        }.to raise_error(/expected block not to perform faster than passed block, but performed faster in \d+.\d+ times/)
+          }.to perform_faster_than { 'x' * 10 * 1024 }.at_most(2).times
+        }.to raise_error(/expected block to perform faster than passed block by at_most \d+ times, but performed faster by \d+.\d+ times/)
       end
     end
+  end
 
-    context "expect { ... }.not_to perform_faster_than(...).at_least(...).times" do
+  context "expect { ... }.not_to perform_faster_than(...)" do
+    it "passes if the block performs slower than sample" do
+      expect { 'x' * 10 * 1024 }.not_to perform_faster_than { 1 << 1 }
+    end
+
+    it "fails if the block performs faster than sample" do
+      expect {
+        expect { 1 << 1 }.not_to perform_faster_than { 'x' * 10 * 1024 }
+      }.to raise_error(/expected block not to perform faster than passed block, but performed faster by \d+.\d+ times/)
+    end
+
+    context 'with at_least count' do
       it "passes if the block performs slower than sample" do
         expect {
           'x' * 10 * 1024
@@ -69,53 +83,23 @@ RSpec.describe RSpec::Benchmark::ComparisonMatcher::Matcher do
           expect {
             1 << 1
           }.not_to perform_faster_than { 'x' * 10 * 1024 }.at_least(2).times
-        }.to raise_error(/expected block not to perform faster than passed block at_least in \d+ times, but performed faster in \d+.\d+ times/)
-      end
-    end
-
-    context "expect { ... }.to perform_faster_than.{ ... }.exactly(...).times" do
-      it "passes if the block performs faster than sample" do
-        expect {
-          1 << 1
-        }.to perform_faster_than { 1 << 1 }.exactly(1).times
-      end
-    end
-
-    context "expect { ... }.to perform_faster_than.{ ... }.at_most(...).times" do
-      it "passes if the block performs faster than sample" do
-        expect {
-          1 << 1
-        }.to perform_faster_than { 'x' * 10 * 1024 }.at_most(125).times
-      end
-
-      it "fails if the block performs faster than sample more than in 20 times" do
-        expect {
-          expect {
-            1 << 1
-          }.to perform_faster_than { 'x' * 10 * 1024 }.at_most(2).times
-        }.to raise_error(/expected block to perform faster than passed block at_most in \d+ times, but performed faster in \d+.\d+ times/)
+        }.to raise_error(/expected block not to perform faster than passed block by at_least \d+ times, but performed faster by \d+.\d+ times/)
       end
     end
   end
 
-  describe '#perform_slower_than' do
-    context "expect { ... }.not_to perform_slower_than(...)" do
-      it "passes if the block performs faster than sample" do
-        expect {
-          1 << 1
-        }.not_to perform_slower_than { 'x' * 10 * 1024 }
-      end
-
-      it "fails if the block performs slower than sample" do
-        expect {
-          expect {
-            'x' * 10 * 1024
-          }.not_to perform_slower_than { 1 << 1 }
-        }.to raise_error(/expected block not to perform slower than passed block, but performed slower in \d+.\d+ times/)
-      end
+  describe "expect { ... }.not_to perform_slower_than(...)" do
+    it "passes if the block performs faster than sample" do
+      expect { 1 << 1 }.not_to perform_slower_than { 'x' * 10 * 1024 }
     end
 
-    context "expect { ... }.not_to perform_slower_than(...).at_least(...).times" do
+    it "fails if the block performs slower than sample" do
+      expect {
+        expect { 'x' * 10 * 1024 }.not_to perform_slower_than { 1 << 1 }
+      }.to raise_error(/expected block not to perform slower than passed block, but performed slower by \d+.\d+ times/)
+    end
+
+    context "with at_least count" do
       it "passes if the block performs faster than sample" do
         expect {
           1 << 1
@@ -127,27 +111,25 @@ RSpec.describe RSpec::Benchmark::ComparisonMatcher::Matcher do
           expect {
             'x' * 10 * 1024
           }.not_to perform_slower_than { 1 << 1 }.at_least(5).times
-        }.to raise_error(/expected block not to perform slower than passed block at_least in \d+ times, but performed slower in \d+.\d+ times/)
+        }.to raise_error(/expected block not to perform slower than passed block by at_least \d+ times, but performed slower by \d+.\d+ times/)
       end
     end
+  end
 
-    context "expect { ... }.not_to perform_slower_than(...)" do
-      it "passes if the block performs slower than sample" do
-        expect {
-          'x' * 10 * 1024
-        }.to perform_slower_than { 1 << 1 }
-      end
-
-      it "fails if the block performs faster than sample" do
-        expect {
-          expect {
-            1 << 1
-          }.to perform_slower_than { 'x' * 10 * 1024 }
-        }.to raise_error(/expected block to perform slower than passed block, but performed faster in \d+.\d+ times/)
-      end
+  describe "expect { ... }.to perform_slower_than(...)" do
+    it "passes if the block performs slower than sample" do
+      expect { 'x' * 10 * 1024 }.to perform_slower_than { 1 << 1 }
     end
 
-    context "expect { ... }.not_to perform_slower_than(...).at_least(...).times" do
+    it "fails if the block performs faster than sample" do
+      expect {
+        expect {
+          1 << 1
+        }.to perform_slower_than { 'x' * 10 * 1024 }
+      }.to raise_error(/expected block to perform slower than passed block, but performed faster by \d+.\d+ times/)
+    end
+
+    context "with at_least count" do
       it "passes if the block does performs slower than sample" do
         expect {
           'x' * 10 * 1024
@@ -159,19 +141,17 @@ RSpec.describe RSpec::Benchmark::ComparisonMatcher::Matcher do
           expect {
             1 << 1
           }.to perform_slower_than { 'x' * 10 * 1024 }.at_least(2).times
-        }.to raise_error(/expected block to perform slower than passed block at_least in \d+ times, but performed faster in \d+.\d+ times/)
+        }.to raise_error(/expected block to perform slower than passed block by at_least \d+ times, but performed faster by \d+.\d+ times/)
       end
     end
 
-    context "expect { ... }.to perform_slower_than.{ ... }.exactly(...).times" do
+    context "with exact count" do
       it "passes if the block performs slower than sample" do
-        expect {
-          1 << 1
-        }.to perform_slower_than { 1 << 1 }.exactly(1).times
+        expect { 1 << 1 }.to perform_slower_than { 1 << 1 }.exactly(1).times
       end
     end
 
-    context "expect { ... }.to perform_slower_than.{ ... }.at_most(...).times" do
+    context "with at_most count" do
       it "passes if the block performs faster than sample" do
         expect {
           'x' * 10 * 1024
@@ -183,7 +163,7 @@ RSpec.describe RSpec::Benchmark::ComparisonMatcher::Matcher do
           expect {
             'x' * 10 * 1024
           }.to perform_slower_than { 1 << 1 }.at_most(2).times
-        }.to raise_error(/expected block to perform slower than passed block at_most in \d+ times, but performed slower in \d+.\d+ times/)
+        }.to raise_error(/expected block to perform slower than passed block by at_most \d+ times, but performed slower by \d+.\d+ times/)
       end
     end
   end
