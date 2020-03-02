57b7537ca3388a76184a5a106fd5f92374f59128
Piotr Murach <pmurach@gmail.com>
2020-03-02 22:59:32 +0000

    Add more test cases to ensure correct behaviour

--- spec/unit/format_time_spec.rb
+++ spec/unit/format_time_spec.rb
@@ -1,18 +1,25 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-RSpec.describe RSpec::Benchmark, '#format_time' do
+RSpec.describe RSpec::Benchmark, "#format_time" do
   {
     1e-10   => "0 ns",
     0.42e-6 => "420 ns",
+    # us
     3.4e-6  => "3.4 μs",
     34e-6   => "34 μs",
     340e-6  => "340 μs",
-    1e-3    => "1 ms",
-    12e-3   => "12 ms",
+    # ms
+    0.001   => "1 ms",
+    0.0123  => "12.3 ms",
+    0.123   => "123 ms",
+    0.1     => "100 ms",
+    # sec
     1.0     => "1 sec",
     1.2345  => "1.23 sec",
+    12.123  => "12.1 sec",
     123.45  => "123 sec",
-    1234    => "1234 sec"
+    1234    => "1234 sec",
+    1234.56 => "1235 sec"
   }.each do |input, expected|
     it "#{input} -> #{expected}" do
       expect(described_class.format_time(input)).to eq(expected)
